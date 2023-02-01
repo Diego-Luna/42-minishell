7afb7ed3f24f6056086e00795070f64aa43ee2b9
Diego francisco Luna lopez <diegofcolunalopez@gmail.com>
2023-02-01 15:13:00 -0500

    exit -5 fixed

--- srcs/ft_minishell/our_commands.c
+++ srcs/ft_minishell/our_commands.c
@@ -6,7 +6,7 @@
 /*   By: dluna-lo <dluna-lo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 14:52:38 by dluna-lo          #+#    #+#             */
-/*   Updated: 2023/01/25 15:01:32 by dluna-lo         ###   ########.fr       */
+/*   Updated: 2023/02/01 15:10:29 by dluna-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void	ft_comand_exit(t_state *state, char **run_comand)
 	state->exit = 0;
 	if (ft_size_table(run_comand) == 2)
 	{
-		if (ft_isdigit_str(run_comand[1]) == 1)
+		if ((run_comand[1][0] == '-' && ft_isdigit_str(run_comand[1] + 1) == 1) || ft_isdigit_str(run_comand[1]) == 1)
 		{
 			state->exit = ft_atoi(run_comand[1]);
 		}
